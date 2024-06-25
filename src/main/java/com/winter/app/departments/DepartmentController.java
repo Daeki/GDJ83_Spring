273be3aa98f719aad0d2f9f81b4107567f662b5a
Daeki <kimdaeki@gmail.com>
2024-06-25 17:48:54 +0900

    1

--- src/main/java/com/winter/app/departments/DepartmentController.java
+++ src/main/java/com/winter/app/departments/DepartmentController.java
@@ -1,7 +1,10 @@
 package com.winter.app.departments;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -16,11 +19,17 @@ public class DepartmentController {
 	private DepartmentService departmentService;
 
 	@RequestMapping(value = "list", method = RequestMethod.GET)
-	public void getList() throws Exception {
+	public void getList(Model model) throws Exception {
 		System.out.println("department list");
 		// return "department/list
-		departmentService.getList();
+		List<DepartmentDTO> ar = departmentService.getList();
 
+		// Model + View
+		// ModelAndView mv = new ModelAndView();
+		// mv.addObject("list", ar);
+		// return mv;
+
+		model.addAttribute("list", ar);
 	}
 
 }
