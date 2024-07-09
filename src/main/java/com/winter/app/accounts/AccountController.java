08c9955fda3e5ff829bda27bd4a97de7d4f98845
Daeki <kimdaeki@gmail.com>
2024-07-09 10:19:49 +0900

    1

--- src/main/java/com/winter/app/accounts/AccountController.java
+++ src/main/java/com/winter/app/accounts/AccountController.java
@@ -4,6 +4,7 @@ import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -23,6 +24,12 @@ public class AccountController {
 		int result = accountService.add(accountDTO);
 
 		return "redirect:/";
+	}
+
+	@RequestMapping(value = "detail", method = RequestMethod.GET)
+	public void detail(AccountDTO accountDTO, Model model) throws Exception {
+		accountDTO = accountService.detail(accountDTO);
+		model.addAttribute("dto", accountDTO);
 	}
 
 }
